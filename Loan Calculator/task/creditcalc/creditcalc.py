e6125f2a69225fcee44b0428f0a4fab7966d51fe
mathew <mathew_holland@hotmail.co.uk>
2026-03-27 18:26:40 +0000

    Stage 1/4 complete

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,1 +1,6 @@
 # write your code here
+loan_principal = float(input("Enter loan principal: "))
+monthly_payment = float(input("Enter monthly payment: "))
+
+time_to_pay = loan_principal / monthly_payment
+print(f"It will take {time_to_pay} months to pay off the loan")
